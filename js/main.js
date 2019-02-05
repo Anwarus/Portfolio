4f182605d6a40a8fe5b990722670927d4caf0471
Anwarus <Anwarus1289@gmail.com>
2019-02-05 19:54:17 +0100

    (improvement) - slide only undiscovered elements

--- js/main.js
+++ js/main.js
@@ -3,9 +3,45 @@ import Circuit from './circuit';
 $(document).ready(function() {
     let canvas = createCanvas('screen', '#target');
     let context = canvas.getContext('2d');
-    let circuit = new Circuit();
-    circuit.generatePaths();
-    circuit.draw(context);
+    //let circuit = new Circuit(canvas);
+
+    function loop() {
+        requestAnimationFrame(loop);
+    
+        context.clearRect(0, 0, canvas.width, canvas.height);
+        circuit.draw(context);
+    
+    }
+
+    var win = $(window);
+    var allMods = $('.slide');
+
+    // Already visible modules
+    allMods.each(function(i, element) {
+        let el = $(element);
+        if (el.discovered()) {
+            el.addClass('already-visible'); 
+        } 
+    });
+
+    win.scroll(function(event) {
+        allMods.each(function(i, element) {
+            let el = $(element);
+            if (el.discovered(true)) {
+                el.addClass('come-in'); 
+            } 
+        });
+    
+    });
+
+    $(window).scroll(function(event) {
+        $('.slide').each(function(i, element) {
+            let el = $(element);
+            if (el.discovered(true)) {
+                el.addClass('come-in'); 
+            } 
+        });
+    });
     
 });
 
