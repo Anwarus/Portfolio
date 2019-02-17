f619e5f7b737a663fa10493af71d05cfefa0616b
Anwarus <Anwarus1289@gmail.com>
2019-02-17 15:15:38 +0100

    (improvement) - improvements in styling

--- js/main.js
+++ js/main.js
@@ -50,6 +50,58 @@ $(document).ready(function() {
     
 });
 
+fetch('https://api.github.com/graphql', {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json', 'Authorization': 'bearer 7f249d17e6a63de60326527e013909682bc30f82' },
+    body: JSON.stringify({ query: 
+        `{
+            user(login: "Anwarus") {
+                repositories(first: 1, orderBy: {field: PUSHED_AT, direction: DESC}) {
+                    nodes {
+                        name
+                        pushedAt
+                        description
+                        languages(first: 10) {
+                            nodes {
+                                name
+                            }
+                        }
+                    }
+                }
+            }
+        }`
+    }),
+})
+    .then(res => res.json())
+    .then(res => {
+        console.log(res.data);
+
+        let card = $('.card-github');
+        let recentRepository = res.data.user.repositories.nodes[0];
+        let lastUpdate = new Date(recentRepository.pushedAt);
+        let stack = (recentRepository.languages.nodes).map(language => language.name);
+
+        $(card).find('.github-date').text(lastUpdate.toISOString().substring(0, 10));
+        $(card).find('.github-stack').text(stack.join('/'));
+        $(card).find('.github-title').text(recentRepository.name);
+        $(card).find('.github-description').text(recentRepository.description);
+
+        $(card).find('.github-button.start').attr('href', 'https://github.com/Anwarus/' + recentRepository.name + '/subscription');
+        $(card).find('.github-button.start').attr('href', 'https://github.com/Anwarus/' + recentRepository.name);
+        $(card).find('.github-button.start').attr('href', 'https://github.com/Anwarus/' + recentRepository.name + '/fork');
+
+        $(card).children('.loader').hide();
+        $(card).children('.content').removeClass('hidden');
+    })
+    .catch(error => {
+        console.log("Error: ", error);
+
+        let card = $('.card-github');
+
+        $(card).children('.loader').hide();
+        $(card).children('.error').removeClass('hidden');
+    });
+
 function createCanvas(id, target) {
     let canvas = document.createElement('canvas');
 
